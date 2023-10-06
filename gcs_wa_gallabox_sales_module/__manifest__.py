45a43edd3aa8ebc92a135f3ed8f66eb2c72fd187
Moula Ali <moulaali@geelani.com>
2023-10-06 17:42:11 +0530

    In this commit updated the module documentation

--- gcs_wa_gallabox_sales_module/__manifest__.py
+++ gcs_wa_gallabox_sales_module/__manifest__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Gcs Whatsapp Gallabox Odoo Module For Sales',
+    'name': 'Gcs WhatsApp Gallabox Odoo Module For Sales',
     "author": "Geelani Consultancy & Solutions, Gallabox",
     'version': '1.0.0',
     "website": "https://gallabox.com/",
     'description': """
         Enhance your Odoo experience with the Gallabox WhatsApp Purchase Order Integration module. This powerful extension seamlessly integrates Gallabox with Odoo, allowing you to effortlessly send purchase order details to customers via WhatsApp. Whether you want to automate the process or send them manually, this module simplifies communication and enhances customer engagement.""",
+    'images': ['static/description/banner.png', 'static/description/icon.png'],
     'category': '',
     'depends': [
         'base'
     ],
-    'images': ['static/description/banner.png', 'static/description/icon.png'],
     'data': [
         'data/gcs_cron.xml',
         'views/gcs_whatsapp_template.xml',
@@ -20,7 +20,7 @@
         "wizard/gcs_whatsapp_composer.xml",
         "security/ir.model.access.csv",
     ],
-    'license' : 'LGPL-3',
+    'license': 'LGPL-3',
     'installable': True,
     'application': True,
     'auto_install': False,
